89074d2b744f72d3f30040cfcc667a94b808793a
Abhishek Tiwari <abhie.lp@gmail.com>
2021-07-25 23:31:20 +0530

    Add user filter to overview queries and fix styling

--- tracker/views.py
+++ tracker/views.py
@@ -30,22 +30,26 @@ def add_expense_view(request):
         .only("amount", "date", "description")[:10]
 
     this_month = Expense.objects.filter(
-        date__month=today.month, date__year=today.year
+        user_id=request.user.id, date__month=today.month, date__year=today.year
     ).aggregate(Sum("amount"))
 
     last_month_date = today.replace(day=1) - timedelta(1)
     last_month = Expense.objects.filter(
-        date__month=last_month_date.month, date__year=last_month_date.year
+        user_id=request.user.id, date__month=last_month_date.month, date__year=last_month_date.year
     ).aggregate(Sum("amount"))
 
     year_month: YearMonth = year_month_before(3)
     last_3_months = Expense.objects.filter(
-        date__month__gte=year_month.month, date__year__gte=year_month.year,
+        user_id=request.user.id, date__month__gte=year_month.month, date__year__gte=year_month.year,
         date__month__lte=last_month_date.month, date__year__lte=last_month_date.year
     ).aggregate(Sum("amount"))
 
-    this_year = Expense.objects.filter(date__year=today.year).aggregate(Sum("amount"))
-    last_year = Expense.objects.filter(date__year=today.year - 1).aggregate(Sum("amount"))
+    this_year = Expense.objects.filter(
+        user_id=request.user.id, date__year=today.year
+    ).aggregate(Sum("amount"))
+    last_year = Expense.objects.filter(
+        user_id=request.user.id, date__year=today.year - 1
+    ).aggregate(Sum("amount"))
     return render(request, "tracker/add.html", {
         "form": form, "previous_10": previous_10, "3_months": last_3_months["amount__sum"],
         "this_year": this_year["amount__sum"], "last_month": last_month["amount__sum"],
