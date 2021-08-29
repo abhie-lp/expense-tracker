f4508fd3ac43238aa78afca2bcf01715d3989b43
Abhishek Tiwari <abhie.lp@gmail.com>
2021-08-30 00:59:01 +0530

    Changes for PROD deployment

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,3 +6,12 @@ from decouple import config
 DEBUG = False
 SECRET_KEY = config("SECRET_KEY")
 ALLOWED_HOSTS = [config("ALLOWED_HOST")]
+
+DATABASES["default"] = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': config("PG_NAME"),
+    'USER': config("PG_USER"),
+    'PASSWORD': config("PG_PASSWORD"),
+    'HOST': config("PG_HOST"),
+    'PORT': config("PG_PORT"),
+}
