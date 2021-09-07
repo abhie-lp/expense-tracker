c897d8d686ecfb0189e0b8ea241861b143451c9b
Abhishek Tiwari <abhie.lp@gmail.com>
2021-09-08 00:16:22 +0530

    Add 4 basic charts

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,10 +1,17 @@
 """URL route mapping for tracker app"""
 
-from django.urls import path
-from .views import expense_history
+from django.urls import path, include
+from .apis import analysis_api
+from .views import expense_history, analysis_view
 
 app_name = "tracker"
 
+api_urls = [
+    path("analysis/", analysis_api, name="api_analysis"),
+]
+
 urlpatterns = [
     path("history/", expense_history, name="history"),
+    path("api/", include(api_urls)),
+    path("analysis/", analysis_view, name="analysis"),
 ]
